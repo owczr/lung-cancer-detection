e6cb45ce14452730f859f891be9636f291ef77dd
Jakub Owczarek <jowczarek@student.agh.edu.pl>
2023-12-28 16:04:37 +0100

    Add BaseModelDirector interface and update ModelDirector

--- src/model/director/model_director.py
+++ src/model/director/model_director.py
@@ -3,6 +3,7 @@ Module for the ModelDirector class
 """
 import logging
 
+from src.model.director.base import BaseModelDirector
 from src.model.builders import ModelBuilder
 
 
@@ -10,27 +11,23 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-class ModelDirector:
+class ModelDirector(BaseModelDirector):
     """Director class for model builders."""
-    def __init__(self, builder: ModelBuilder):
-        self.__builder = builder
-        logger.info(f"Initialized ModelDirector with {builder}")
-
     def make(self):
         """Builds and returns the built model."""
-        self.__builder.reset()
-        self.__builder.set_preprocessing_layers()
-        self.__builder.set_model_layers()
-        self.__builder.set_output_layers()
-        return self.__builder.build()
+        self._builder.reset()
+        self._builder.set_preprocessing_layers()
+        self._builder.set_model_layers()
+        self._builder.set_output_layers()
+        return self._builder.build()
 
     @property
     def builder(self):
         """ModelBuilder object property"""
-        return self.__builder
+        return self._builder
 
     @builder.setter
     def builder(self, builder: ModelBuilder):
         """Sets the ModelBuilder object property"""
-        self.__builder = builder
-        logger.info(f"ModelDirector builder set to {builder}")
+        self._builder = builder
+        logger.info(f"ModelDirector builder set to {str(builder)}")
