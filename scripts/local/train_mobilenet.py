9fc19e78b9869373cb6c7c96328e3ff1e947cc75
Jakub Owczarek <jowczarek@student.agh.edu.pl>
2023-12-28 16:06:35 +0100

    Update MobileNet training script

--- scripts/local/train_mobilenet.py
+++ scripts/local/train_mobilenet.py
@@ -1,16 +1,26 @@
 from src.model.builders import MobileNetBuilder
 from src.model.director import ModelDirector
+from src.dataset.dataset_loader import DatasetLoader 
+
+
+DATASET_DIR = "LIDC-IDRI/CT/processed/train"
 
 
 def build_mobilenet():
     builder = MobileNetBuilder()
     director = ModelDirector(builder)
+    
     model = director.make()
     return model
 
 
 def run():
     model = build_mobilenet()
+    loader = DatasetLoader(DATASET_DIR)
+    dataset = loader.get_dataset()
+
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+    model.fit(dataset)
 
 
 if __name__ == "__main__":
