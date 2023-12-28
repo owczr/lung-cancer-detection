60a6eda87dc98e7ed7e4cbd841c2bf62d8d2c3ca
Jakub Owczarek <jowczarek@student.agh.edu.pl>
2023-12-28 17:49:05 +0100

    Add XCeption builder, tests and training script

--- src/model/builders/__init__.py
+++ src/model/builders/__init__.py
@@ -2,3 +2,4 @@ from .base import ModelBuilder
 from .mobilenet import MobileNetBuilder
 from .efficientnet import EfficientNetBuilder
 from .inceptionnet import InceptionNetBuilder
+from .xception import XceptionBuilder
