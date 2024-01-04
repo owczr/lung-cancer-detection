e96cabb63044397b8e1a85837ada230cf32dfdb0
Jakub Owczarek <jowczarek@student.agh.edu.pl>
2024-01-04 23:21:18 +0100

    Add scripts to train model and submit a job on Azure ML

--- src/config.py
+++ src/config.py
@@ -2,10 +2,37 @@ import logging
 from datetime import datetime
 
 # Get the current date to create a dynamic log filename
-current_date = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-log_filename = f'lung_cancer_detection_{current_date}.log'
+current_date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+log_filename = f"lung_cancer_detection_{current_date}.log"
 logging.basicConfig(
     filename=log_filename,
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
+    format="%(asctime)s - %(levelname)s - %(message)s",
 )
+
+EARLY_STOPPING_CONFIG = {
+    "monitor": "val_loss",
+    "min_delta": 0.001,
+    "patience": 3,
+}
+
+REDUCE_LR_CONFIG = {
+    "monitor": "val_loss",
+    "factor": 0.1,
+    "patience": 2,
+    "min_delta": 0.001,
+}
+
+MODELS = [
+    "convnext",
+    "densenet",
+    "efficientnet",
+    "efficientnetv2",
+    "inceptionnet",
+    "inceptionresnet",
+    "mobilenet",
+    "resnet",
+    "resnetv2",
+    "vgg",
+    "xception",
+]
